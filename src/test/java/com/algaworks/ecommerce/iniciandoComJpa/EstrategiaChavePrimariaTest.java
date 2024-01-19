39ff863fb7514cfe12d2ceabced2bd0c49562ff6
Lucas <lucasdecassia9@hotmail.com>
2024-01-19 17:47:03 -0300

    entendo o hibernate gerando os ID com AUTO, no banco de dados

---
+++ src/test/java/com/algaworks/ecommerce/iniciandoComJpa/EstrategiaChavePrimariaTest.java
@@ -0,0 +1,27 @@
+package com.algaworks.ecommerce.iniciandoComJpa;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Util.Model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EstrategiaChavePrimariaTest extends EntityManagerTest {
+
+    @Test
+    public void testarEstrategiaAuto(){
+        Categoria categoria = new Categoria();
+        categoria.setNome("Eletronicos");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+
+        Assert.assertNotNull(categoriaVerificacao);
+
+    }
+
+}
