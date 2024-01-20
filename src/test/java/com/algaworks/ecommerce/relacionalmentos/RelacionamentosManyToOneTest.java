25ccc93da49057b6b07b2be7dd9ee0c427a29e66
Lucas <lucasdecassia9@hotmail.com>
2024-01-20 18:00:08 -0300

    relacionanmeto cliente e produto

---
+++ src/test/java/com/algaworks/ecommerce/relacionalmentos/RelacionamentosManyToOneTest.java
@@ -0,0 +1,41 @@
+package com.algaworks.ecommerce.relacionalmentos;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.Util.Model.Cliente;
+import com.algaworks.ecommerce.model.Util.Model.Pedido;
+import com.algaworks.ecommerce.model.Util.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RelacionamentosManyToOneTest  extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento(){
+       Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        Pedido pedido = new Pedido();
+
+        pedido.setStatusPedido(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+
+        pedido.setCliente(cliente);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getCliente());
+
+
+
+    }
+
+
+}
